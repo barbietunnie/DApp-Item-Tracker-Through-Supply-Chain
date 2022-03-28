a88e2ea47ba64205c79a46c7d397f8340dd2809b
Babatunde Adeyemi <barbietunnie@gmail.com>
2022-03-29 00:53:39 +0100

    implement sell item

--- project-6/contracts/coffeebase/SupplyChain.sol
+++ project-6/contracts/coffeebase/SupplyChain.sol
@@ -217,14 +217,16 @@ contract SupplyChain {
   // Define a function 'sellItem' that allows a farmer to mark an item 'ForSale'
   function sellItem(uint _upc, uint _price) public 
   // Call modifier to check if upc has passed previous supply chain stage
-  
+  packed(_upc)
   // Call modifier to verify caller of this function
-  
+  verifyCaller(owner)
   {
     // Update the appropriate fields
+    items[_upc].productPrice = _price;
+    items[_upc].itemState = State.ForSale;
     
     // Emit the appropriate event
-    
+    emit Sold(_upc);
   }
 
   // Define a function 'buyItem' that allows the disributor to mark an item 'Sold'
