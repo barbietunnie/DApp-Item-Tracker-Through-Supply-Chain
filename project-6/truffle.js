7a8ffefaf6a8dccb0fd345fc6b15eef3c0e43691
Babatunde Adeyemi <barbietunnie@gmail.com>
2022-03-30 03:59:49 +0100

    Fix web3 import

--- project-6/truffle.js
+++ project-6/truffle.js
@@ -1,10 +1,40 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const fs = require("fs");
+
+const infuraKey = fs.readFileSync(".infura").toString().trim();
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 8545,
+      port: 9545,
       network_id: "*" // Match any network id
-    }
+    },
+    rinkeby: {
+      provider: () =>
+        new HDWalletProvider(
+          mnemonic,
+          `https://rinkeby.infura.io/v3/${infuraKey}`
+        ),
+      network_id: 4,
+      gas: 6500000,
+      gasPrice: 25000000000,
+      skipDryRun: true,
+      transactionBlockTimeout: 2000,
+    },
+    ropsten: {
+      provider: () =>
+        new HDWalletProvider(
+          mnemonic,
+          `https://ropsten.infura.io/v3/${infuraKey}`
+        ),
+      network_id: 3,
+      gas: 6500000,
+      gasPrice: 25000000000,
+      skipDryRun: true,
+      transactionBlockTimeout: 2000,
+    },
   },
   compilers: {
     solc: {
